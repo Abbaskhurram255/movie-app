914782e9e9fba6e8d0cf1d8b5794253d9e409562
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-06 03:07:42 +0500

    Updates

---
+++ precache-manifest.b48a1f63b2325b47a557e8cb12811626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621c579609df65e43155b7988ccb363b",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "73912b2f682652ca3722",
+    "url": "/movie-app/static/css/main.d9180a40.chunk.css"
+  },
+  {
+    "revision": "75d33a0d46d6f5e6a0b6",
+    "url": "/movie-app/static/js/2.919ce5ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.919ce5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73912b2f682652ca3722",
+    "url": "/movie-app/static/js/main.6a0f8bd7.chunk.js"
+  },
+  {
+    "revision": "243ec900a6d4d156aaee",
+    "url": "/movie-app/static/js/runtime-main.cda3830b.js"
+  },
+  {
+    "revision": "5eb0b8830d1a853ce96e4c6e44994a44",
+    "url": "/movie-app/static/media/no-image.5eb0b883.jpg"
+  }
+]);
